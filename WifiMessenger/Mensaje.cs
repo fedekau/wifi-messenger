71d9f4e469b4d872ba4e9ff672c225e65e6d30b1
anacesponda <anacesponda@gmail.com>
2015-04-18 11:29:15 -0300

    Interfaz Grafica

---
+++ WifiMessenger/Mensaje.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+namespace WifiMessenger
+{
+	public class Mensaje
+	{
+		public Mensaje(string msjIzq, string msjDer,String boxColor, LayoutOptions ubic, LayoutOptions u1, LayoutOptions u2)
+		{
+			this.MensajeIzq = msjIzq;
+			this.MensajeDer= msjDer;
+			this.BoxColor = boxColor;
+			this.ubicBox = ubic;
+			this.ubicIzq = u1;
+			this.ubicDer = u2;
+		}
+
+		public string MensajeIzq { private set; get; }
+		public string MensajeDer { private set; get; }
+		public LayoutOptions ubicBox { private set; get; }
+		public LayoutOptions ubicIzq { private set; get; }
+		public LayoutOptions ubicDer { private set; get; }
+		public String BoxColor { private set; get; }
+	};
+}
+
--- WifiMessenger/WifiMessenger.cs
+++ WifiMessenger/WifiMessenger.cs
@@ -2,7 +2,10 @@
 
 using Xamarin.Forms;
 using Dolphins.Salaam;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Net;
+using System.Net.Sockets;
 
 namespace WifiMessenger
 {
@@ -10,10 +13,92 @@ namespace WifiMessenger
 	{
 		private SalaamService service = new SalaamService ("wifi_msg", "WifiMessenger", 15000);
 		private SalaamBrowser browser = new SalaamBrowser();
+		private IPAddress ip;
+		private bool first;
+		//ScrollView display chat messages
 		private Label result = new Label {
 			XAlign = TextAlignment.Center,
 			Text = "Welcome to Xamarin Forms!"
 		};
+
+		// Lista de mensajes <Observable Collection> para que cuando cambie el ItemSource la listView se actualice sola
+		public ObservableCollection<Mensaje> mensajes = new ObservableCollection<Mensaje>();
+		// Create the ListView.
+		ListView historial = new ListView
+		{
+			// Source of data items.
+			//ItemsSource = mensajes,
+
+			// Define template for displaying each item.
+			// (Argument of DataTemplate constructor is called for 
+			//      each item; it must return a Cell derivative.)
+
+			ItemTemplate = new DataTemplate(() =>
+				{
+					// Create views with bindings for displaying each property.
+					//Creo dos etiquetas para que dependiendo de quien lo escriba (misma ip o no) como se muestre
+
+					//Defino etiqueta izquierda
+					Label nameLabelizq = new Label();
+					nameLabelizq.SetBinding(Label.TextProperty, "MensajeIzq");
+					//Defino etiqueta derecha
+					Label nameLabelder = new Label();
+					nameLabelder.SetBinding(Label.TextProperty, "MensajeDer");
+					//Defino 
+					Image img = new Image { Aspect = Aspect.AspectFit };
+					img.SetBinding(Image.SourceProperty, "BoxColor");
+					img.SetBinding(Image.HorizontalOptionsProperty, "ubicBox");
+
+					//Stack msj izq
+					StackLayout st1= new StackLayout();
+					st1.VerticalOptions=LayoutOptions.Center;
+					st1.Spacing=0;
+					st1.Children.Add(nameLabelizq);
+					//Stack msj 
+					StackLayout st2= new StackLayout();
+					st2.VerticalOptions=LayoutOptions.Center;
+					st2.Spacing=0;
+					st2.Children.Add(nameLabelder);
+					//st1.SetBinding(Layout.HorizontalOptionsProperty, "ubicIzq");
+					st1.SetBinding(Layout.HorizontalOptionsProperty, "ubicDer");
+
+					// Return an assembled ViewCell.
+					return new ViewCell
+					{
+						View = new StackLayout
+						{
+							Padding = new Thickness(0, 5),
+							Orientation = StackOrientation.Horizontal,
+							Children = 
+							{
+								st1,
+								img,
+								st2,
+
+							}
+							}
+					};
+
+				}),
+
+		};
+
+
+
+
+
+		Button BotonEnviar = new Button
+		{
+			Text = "Enviar!",
+			Font = Font.SystemFontOfSize(NamedSize.Large),
+			BorderWidth = 1,
+			HorizontalOptions = LayoutOptions.Center,
+			//BackgroundColor = Color.Blue,
+
+		};
+		Entry entryCell = new Entry(){
+			Placeholder = "Escriba su mensaje aquí"
+		} ;
 
 		public App ()
 		{
@@ -24,32 +109,65 @@ namespace WifiMessenger
 				Content = new StackLayout {
 					VerticalOptions = LayoutOptions.Center,
 					Children = {
-						result,
-					}
+						historial,
+						entryCell,
+						BotonEnviar
+					},
 
 				}
+
 			};
+			first = true;
 		}
 
 		protected override void OnStart ()
 		{
 			// Handle when your app starts
 
-
 			service.Registered += HandleRegistered;
 			service.CreationFailed += HandleCreationFailed;
 			service.BroadcastFailed += HandleBroadcastFailed;
 			service.Unregistered += HandleUnregistered;
-
+			//CARGO MI IP
+			ip = LocalIPAddress ();
 			service.Message = "READY_TO_CHAT";
 
 			service.Register ();
+
+			BotonEnviar.Clicked += (sender, e) => { 
+				System.Console.WriteLine (entryCell.Text);
+
+				service.Message=entryCell.Text;
+				entryCell.Text=null;
+			};
+
+			historial.ItemSelected+= (sender, e) => { 
+				historial.SelectedItem=null;
+			};
+
+
 
 		}
 
 		protected override void OnSleep ()
 		{
 			// Handle when your app sleeps
+		}
+
+		public IPAddress LocalIPAddress()
+		{
+			IPHostEntry host;
+			IPAddress localIP=null;
+			host = Dns.GetHostEntry(Dns.GetHostName());
+			foreach (IPAddress ip in host.AddressList)
+			{
+				if (ip.AddressFamily == AddressFamily.InterNetwork)
+				{
+					localIP = ip;
+					break;
+				}
+			}
+			return localIP;
 		}
 
 		protected override void OnResume ()
@@ -61,7 +179,28 @@ namespace WifiMessenger
 			result.Text = "Se detecto cambio en mensaje de cliente " + e.Client.HostName;
 			System.Console.WriteLine ("[Salaam Browser] "+ DateTime.Now +" Se detecto cambio en mensaje de cliente:");
 			System.Console.WriteLine ("\t"+"Cliente: " + e.Client.HostName + ":" + e.Client.Port);
-			System.Console.WriteLine ("\t"+"Nuevo mensaje: " + e.Client.Message);	
+			System.Console.WriteLine ("\t"+"Nuevo mensaje: " + e.Client.Message);
+			if (e.Client.Address.Equals(ip)) {
+				Mensaje m = new Mensaje ( e.Client.Message,"", "@drawable/enviado", LayoutOptions.End, LayoutOptions.End, LayoutOptions.EndAndExpand);
+				mensajes.Add (m);
+				//UPDATE DE MENSAJES 
+				historial.ItemsSource=null;
+				historial.ItemsSource = mensajes;
+
+				// VOY AL FINAL DE LA CONVERSACIÓN
+				historial.ScrollTo(m, ScrollToPosition.End, true );
+			} else {
+				Mensaje m = new Mensaje ("", e.Client.Message, "@drawable/recibido", LayoutOptions.Start, LayoutOptions.Start, LayoutOptions.Start);
+				mensajes.Add (m);
+				//UPDATE DE MENSAJES 
+				historial.ItemsSource=null;
+				historial.ItemsSource = mensajes;
+
+				// VOY AL FINAL DE LA CONVERSACIÓN
+				historial.ScrollTo(m, ScrollToPosition.End, true );
+			}
+
+
 		}
 
 		private void HandleClientDisappeared (object sender, SalaamClientEventArgs e){
@@ -72,10 +211,18 @@ namespace WifiMessenger
 		}
 
 		private void HandleClientAppeared (object sender, SalaamClientEventArgs e){
-			result.Text = "Un cliente ha aparecido " + e.Client.HostName;
+			result.Text= result.Text + "Un cliente ha aparecido " + e.Client.HostName;
 			System.Console.WriteLine ("[Salaam Browser] "+ DateTime.Now +" Un cliente ha aparecido:");
 			System.Console.WriteLine ("\t"+"Cliente: " + e.Client.HostName + ":" + e.Client.Port);
-			System.Console.WriteLine ("\t"+"Mensaje: " + e.Client.Message);	
+			System.Console.WriteLine ("\t"+"Mensaje: " + e.Client.Message);
+			//Xamarin.listView no se actualiza bien en el primer item por eso tuve que hacer esto
+			if (first) {
+				Mensaje m = new Mensaje ("", "", "@drawable/transparente", LayoutOptions.Start, LayoutOptions.Start, LayoutOptions.Start);
+				mensajes.Add (m);
+				historial.ItemsSource = mensajes;
+				first = false;
+			}
+
 		}
 
 		private void HandleBrowserFailed (object sender, EventArgs e){
@@ -86,6 +233,7 @@ namespace WifiMessenger
 		private void HandleStarted (object sender, EventArgs e){
 			result.Text = "El buscador se ha iniciado correctamente..";
 			System.Console.WriteLine ("[Salaam Browser] "+ DateTime.Now +" El buscador se ha iniciado correctamente");
+			//			historial.ItemsSource=null;
 		}
 
 		private void HandleStartFailed (object sender, EventArgs e){
@@ -121,10 +269,9 @@ namespace WifiMessenger
 			//Esto en un futuro deberia ser false, se usa en true para "ver mas dispositivos"
 			//Ya que soy pobre y tengo solo un cel como la gente, y uno viejo que tenia tirado ahi
 			browser.ReceiveFromLocalMachine = true;
-
 			browser.Start ("wifi_msg");
 		}
-			
+
 		private void HandleCreationFailed(object sender, EventArgs e){
 			result.Text = "Ocurrio un error al iniciar el servicio..";
 			System.Console.WriteLine ("[Salaam Service] "+ DateTime.Now +" Ocurrio un error al iniciar el servicio");
@@ -139,6 +286,8 @@ namespace WifiMessenger
 			result.Text = "Servicio terminado correctamente..";
 			System.Console.WriteLine ("[Salaam Service] "+ DateTime.Now +" Servicio terminado correctamente");
 		}
+
+
 	}
 }
 
